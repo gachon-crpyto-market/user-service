1ceb0e4868e1c7fcb45f72f6eb09caff3ce1ee81
RyuKwanKon <rhks33rhs@gmail.com>
2023-10-31 16:26:17 +0900

    [init] project 생성

---
+++ src/test/java/com/example/gachoncrpytouserservice/GachonCrpytoUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gachoncrpytouserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachonCrpytoUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
