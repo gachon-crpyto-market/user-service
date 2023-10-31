817c620f3d113c750a8d6bde2d0edd4a54fee170
RyuKwanKon <rhks33rhs@gmail.com>
2023-10-31 17:57:09 +0900

    [feat] #1 BusinessException을 추상화해 UnauthorizedException 구현

---
+++ src/main/java/com/example/gachoncrpytouserservice/global/error/exception/UnauthorizedException.java
@@ -0,0 +1,13 @@
+package com.example.gachoncrpytouserservice.global.error.exception;
+
+import com.example.gachoncrpytouserservice.global.error.ErrorCode;
+
+public class UnauthorizedException extends BusinessException {
+    public UnauthorizedException() {
+        super(ErrorCode.UNAUTHORIZED);
+    }
+
+    public UnauthorizedException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
